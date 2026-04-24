d2b02f3dd4f429ed9bf1918ba1be480ab1dcb1f4
deepalijain <deeps19nija@gmail.com>
2026-04-24 20:28:05 +0530

    sprint5

---
+++ terraform/monitoring/main.tf
@@ -0,0 +1,14 @@
+resource "helm_release" "monitoring" {
+  name       = "monitoring"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  namespace  = "monitoring"
+  create_namespace = true
+}
+resource "helm_release" "kubecost" {
+  name       = "kubecost"
+  repository = "https://kubecost.github.io/cost-analyzer/"
+  chart      = "cost-analyzer"
+  namespace  = "kubecost"
+  create_namespace = true
+}
